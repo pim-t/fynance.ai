85dfe9be5e4c65c322029205c74d0e18bc3f4744
pimentoss <piraya.qt@gmail.com>
2019-11-30 19:59:07 +1100

    front page
    super janky LMAO

--- fynance-app/src/App.js
+++ fynance-app/src/App.js
@@ -4,23 +4,14 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Footer from './StickyFooter'; 
 import Avatar from '@material-ui/core/Avatar';
+import Grid from './gridsplash'; 
 
 
 function App() {
   return (
     <div>
-    <div
-    style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }}
-    >
-      <center> <Avatar>H</Avatar> </center> <br/>
-      <Button variant="contained" color="primary">
-    INVESTOR 
-  </Button> <Button variant="contained" color="primary">
-    INVESTEE 
-  </Button> </div>
+     
+  <Grid/>
     
     <Footer/>
     </div>
